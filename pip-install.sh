e24989b722d941275a24bac72830b108dd733f27
Josh <jedagda@miners.utep.edu>
2020-03-28 12:13:53 -0600

    Corrected the pip-install script

--- pip-install.sh
+++ pip-install.sh
@@ -1,11 +1,8 @@
 VAGRANT_FILE_PATH=./vagrant/src/
-
+cd $VAGRANT_FILE_PATH
 pip3 t- $VAGRANT_FILE_PATH install \
     flask \
     requests \
     request \
     requests \
-    unique_id \
-    json \
-    re 
-
+    unique_id 
