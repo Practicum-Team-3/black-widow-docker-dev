b2f3b97d341bca3e3bc52c63668a1b626dc1e052
Mijacu <mijacu_018@hotmail.com>
2020-04-04 23:17:59 +0000

    Documentation was updated

--- black_widow/main_server/docs/conf.py
+++ black_widow/main_server/docs/conf.py
@@ -24,7 +24,7 @@ copyright = u'2020, Software Practicum Team 3'
 author = u'Software Practicum Team 3'
 
 # The short X.Y version
-version = u''
+version = u'0.3'
 # The full version, including alpha/beta/rc tags
 release = u'1'
 
@@ -42,8 +42,10 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.coverage',
     'sphinx.ext.viewcode',
+    'rst2pdf.pdfbuilder'
 ]
 
+pdf_documents = [('index', u'Automated Malware and Exploit Data Collection Tool Documentation', u'Automated Malware and Exploit Data Collection Tool Documentation', u'Software Practicum Team 3')]
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
@@ -88,7 +90,7 @@ html_theme = 'sphinx_rtd_theme'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+#html_static_path = ['_static']
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
