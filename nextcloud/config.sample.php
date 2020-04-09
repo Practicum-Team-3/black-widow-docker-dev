37255b58c2d48f8922a628d92f4a0895783019b7
Josh Dagda <jedagda@gmail.com>
2020-04-09 04:02:29 +0000

    Enabled nextcloudn

---
+++ nextcloud/config.sample.php
@@ -0,0 +1,38 @@
+<?php
+$CONFIG = array (
+  'htaccess.RewriteBase' => '/',
+  'memcache.local' => '\\OC\\Memcache\\APCu',
+  'apps_paths' => 
+  array (
+    0 => 
+    array (
+      'path' => '/var/www/html/apps',
+      'url' => '/apps',
+      'writable' => false,
+    ),
+    1 => 
+    array (
+      'path' => '/var/www/html/custom_apps',
+      'url' => '/custom_apps',
+      'writable' => true,
+    ),
+  ),
+  'passwordsalt' => 'n2CI6ilfo713hTzo1c4PX35NuE/Mvm',
+  'secret' => 'Xo5vYkGneo+0SkcXnnbzvMzRukB4R0O2Ft6W9wK6OEeXfoq4',
+  'trusted_domains' => 
+  array (
+    0 => '172.18.128.1', 'localhost'
+  ),
+  'datadirectory' => '/var/www/html/data',
+  'dbtype' => 'pgsql',
+  'version' => '18.0.3.0',
+  'overwrite.cli.url' => 'http://localhost',
+  'dbname' => 'files',
+  'dbhost' => 'nextcloud_DB',
+  'dbport' => '',
+  'dbtableprefix' => 'oc_',
+  'dbuser' => 'oc_admin',
+  'dbpassword' => '4uiecxugunyy9re4lmiu80qhwnj10g',
+  'installed' => true,
+  'instanceid' => 'oce7pcrfspxk',
+);
