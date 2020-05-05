b089807693156dbc9db40c1e01bf7916587bd4a3
mijacu <mijacu_018@hotmail.com>
2020-05-05 15:43:02 -0600

    Saltstack part IV.II docker compose folders updated

--- run.sh
+++ run.sh
@@ -4,8 +4,8 @@ VAGRANT_DIR=$WORKING_DIR/vagrant/vagrant_server/src/
 
 docker-compose down
 docker network prune -f
-docker-compose up mongodb redis black_widow
-#gnome-terminal -- bash -c "docker-compose up mongodb redis black_widow"
-#gnome-terminal -- bash -c "cd $VAGRANT_DIR; celery worker -A CeleryWorker.celery --loglevel=info"
-#gnome-terminal -- bash -c "cd $VAGRANT_DIR; python3 VagrantServer.py"
+#docker-compose up mongodb redis black_widow
+gnome-terminal -- bash -c "docker-compose up"
+gnome-terminal -- bash -c "cd $VAGRANT_DIR; celery worker -A CeleryWorker.celery --loglevel=info"
+gnome-terminal -- bash -c "cd $VAGRANT_DIR; python3 VagrantServer.py"
 
